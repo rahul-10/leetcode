6db364e7d976eb845604850b70823814941310f7
Rahul Dubey <rahuldubey010@gmail.com>
2025-09-14 11:43:08 +0530

    Time: 65 ms (13.00%) | Memory: 18.2 MB (12.22%) - LeetSync

---
+++ 3-longest-substring-without-repeating-characters/longest-substring-without-repeating-characters.py
@@ -0,0 +1,17 @@
+class Solution:
+    def lengthOfLongestSubstring(self, s: str) -> int:
+        map = {}
+
+        starting_index=0
+        max_length = 0
+        for index in range(0, len(s)):
+            if s[index] in map and map[s[index]] >= starting_index:
+                starting_index = map[s[index]] + 1
+            print('starting_index: ', starting_index, ', index: ', index)
+            map[s[index]] = index
+            if (index - starting_index + 1) > max_length:
+                max_length = index - starting_index + 1
+
+        return max_length
+            
+        
