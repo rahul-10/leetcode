9f84da43a734ac19ed528e4fcc90ae829600ad70
Rahul Dubey <rahuldubey010@gmail.com>
2025-09-30 21:40:03 +0530

    Time: 44 ms (51.41%) | Memory: 17.7 MB (85.64%) - LeetSync

--- 377-combination-sum-iv/combination-sum-iv.py
+++ 377-combination-sum-iv/combination-sum-iv.py
@@ -1,23 +1,31 @@
 class Solution:
-    def combinationSum4Util(self, nums: List[int], target: int, dp: List[int]) -> int:
-        if target < 0:
-            return 0
-        if target == 0:
-            return 1
+    # def combinationSum4Util(self, nums: List[int], target: int, dp: List[int]) -> int:
+    #     if target < 0:
+    #         return 0
+    #     if target == 0:
+    #         return 1
         
-        if dp[target] >= 0:
-            return dp[target]
+    #     if dp[target] >= 0:
+    #         return dp[target]
 
-        current_sum = 0
+    #     current_sum = 0
         
-        for i in range(0, len(nums)):
-            current_sum += self.combinationSum4Util(nums, target-nums[i], dp)
+    #     for i in range(0, len(nums)):
+    #         current_sum += self.combinationSum4Util(nums, target-nums[i], dp)
         
-        dp[target] = current_sum
-        return current_sum
+    #     dp[target] = current_sum
+    #     return current_sum
 
     def combinationSum4(self, nums: List[int], target: int) -> int:
         dp = []
         for i in range(0, target+1):
-            dp.append(-1)
-        return self.combinationSum4Util(nums, target, dp)
+            dp.append(0)
+        # return self.combinationSum4Util(nums, target, dp)
+        dp[0] = 1
+        for index in range(1, target+1):
+            current_sum = 0
+            for i in range(0, len(nums)):
+                current_sum += dp[index-nums[i]] if index-nums[i] >= 0 else 0
+            dp[index] = current_sum
+
+        return dp[target]
