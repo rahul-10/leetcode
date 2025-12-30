da031a4bea09f06a1aa3e673e27afeb5140543fd
Rahul Dubey <rahuldubey010@gmail.com>
2025-12-30 06:08:04 +0530

    Time: 0 ms (100.00%) | Memory: 17.2 MB (95.16%) - LeetSync

--- 199-binary-tree-right-side-view/binary-tree-right-side-view.py
+++ 199-binary-tree-right-side-view/binary-tree-right-side-view.py
@@ -1,3 +1,5 @@
+from collections import deque
+
 # Definition for a binary tree node.
 # class TreeNode:
 #     def __init__(self, val=0, left=None, right=None):
@@ -5,24 +7,23 @@
 #         self.left = left
 #         self.right = right
 class Solution:
-    def __init__(self):
-        self.max_hight = -1
-
-    def rightSigheViewUtil(self, root:Optional[TreeNode], curr_height: int, right_view: List[int]) -> None:
-        if not root:
-            return 
-        
-        if self.max_hight < curr_height:
-            self.max_hight = curr_height
-            right_view.append(root.val)
-        
-        self.rightSigheViewUtil(root=root.right, curr_height=curr_height+1, right_view=right_view)
-
-        self.rightSigheViewUtil(root=root.left, curr_height=curr_height+1, right_view=right_view)
-
-
     def rightSideView(self, root: Optional[TreeNode]) -> List[int]:
-        right_view = []
-        self.rightSigheViewUtil(root, 0, right_view)
-        return right_view
+        result = []
+
+        if not root:
+            return result
+
+        queue = deque()
+        queue.append(root)
+        while queue:
+            result.append(queue[0].val)
+            for i in range(len(queue)):
+                temp = queue.popleft()
+                if temp.right:
+                    queue.append(temp.right)
+                if temp.left:
+                    queue.append(temp.left)
+
+        return result
+
         
