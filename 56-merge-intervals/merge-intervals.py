8459cd3878b8820038c29e5c22a2487e0021df86
Rahul Dubey <rahuldubey010@gmail.com>
2026-01-05 00:37:08 +0530

    Time: 7 ms (74.36%) | Memory: 20.4 MB (91.12%) - LeetSync

--- 56-merge-intervals/merge-intervals.py
+++ 56-merge-intervals/merge-intervals.py
@@ -6,8 +6,9 @@ class Solution:
         for _, val in enumerate(intervals[1:]):
             if val[0] > res[-1][1]:
                 res.append(val)
-            else:
-                res[-1][1] = max(res[-1][1], val[1])\
+                continue
+            
+            res[-1][1] = max(res[-1][1], val[1])\
 
         return res
 
