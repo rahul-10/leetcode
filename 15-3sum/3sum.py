34684f5e4fbabf924ce39231097ab57850cb3ace
Rahul Dubey <rahuldubey010@gmail.com>
2026-01-05 23:34:24 +0530

    Time: 865 ms (20.02%) | Memory: 20.3 MB (76.54%) - LeetSync

--- 15-3sum/3sum.py
+++ 15-3sum/3sum.py
@@ -1,40 +1,29 @@
 class Solution:
-    def can_append(self, x, y, z, result_map) -> bool:
-        nums = [x, y, z]
-        nums.sort()
-        
-        if result_map.get(nums[0], None) is None:
-            result_map[nums[0]] = {}
-
-        if result_map.get(nums[0]).get(nums[1]) == nums[2]:
-            return False
-
-        
-        result_map[nums[0]][nums[1]] = nums[2]
-        return True
-        
     def threeSum(self, nums: List[int]) -> List[List[int]]:
         result = []
-        result_map = {}
         nums.sort()
-        is_processed = {}
-        for i in range(len(nums)):
-            if is_processed.get(i, None) is None:
-                is_processed[i]= {} 
-
+        for i in range(len(nums)-2):
             if i >= 1 and nums[i] == nums[i-1]:
                 continue
-            hash_map = {}
-            for j in range(i+1, len(nums)):
-                if is_processed[i].get(j, None) == True:
-                    continue
-                k = -1 * (nums[i] + nums[j]) 
-                if k in hash_map and self.can_append(nums[i], nums[j], k, result_map):
-                    is_processed[i][j] = True
-                    result.append([nums[i], nums[j], k])
+            left = i + 1
+            right = len(nums)-1
+            while left < right:
                 
-                hash_map[nums[j]] = j
-        
+                if nums[left] + nums[right] + nums[i] == 0:
+                    result.append([nums[i], nums[left], nums[right]])
+                    left += 1
+                    right -= 1
+                    
+                    while left < len(nums) and nums[left] == nums[left-1]:
+                        left += 1
+
+                    while right >= 0 and nums[right] == nums[right+1]:
+                        right -= 1
+
+                elif nums[left] + nums[right] + nums[i] > 0:
+                    right = right-1
+                else:
+                    left += 1
         return result
 
 
